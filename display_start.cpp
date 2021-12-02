7b3d60cc29f15bd0491fe513bce6bdb9ef3711b3
Wreck200808 <80967502+Wreck200808@users.noreply.github.com>
2021-12-02 22:57:29 +0800

    Delete display_start.cpp

--- display_start.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include "display_start.h"
-using namespace std;
-void display_start(){
-    cout << "Welcome to our 5X5 2048!" << endl;
-    cout << "'p' stands for play and 'e' stands for exit." << endl;
-    cout << "type in your choice and click 'enter' on your kerboard: ";
-}
